edaa272ca73db79310a1aef297dd5a8b4de1b338
ghxuan <ghxuan@outlook.com>
2018-12-03 10:53:50 +0800

    加一

---
+++ py/plus-one.py
@@ -0,0 +1,13 @@
+
+def plusOne(digits):
+    """
+    :type digits: List[int]
+    :rtype: List[int]
+    """
+    return int(''.join([str(i) for i in digits])) + 1
+
+
+print(plusOne([1, 2, 3]))
+print(plusOne([4, 3, 2, 1]))
+# 124
+# 4322
