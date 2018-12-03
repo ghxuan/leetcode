748a861cc7df3b92edfecc24c5190d2b9506aa14
ghxuan <ghxuan@outlook.com>
2018-12-03 10:41:55 +0800

    复数乘法

---
+++ py/complex-number-multiply.py
@@ -0,0 +1,31 @@
+
+def complex_number_multiply(a, b):
+    """
+    :type a: str
+    :type b: str
+    :rtype: str
+    """
+    return number_to_complex(complex_to_numbers(a)*complex_to_numbers(b))
+
+
+def complex_to_numbers(s):
+    """
+    :param s: str
+    :return: complex
+    """
+    a, b = s.split('+')
+    return int(a)+int(b.replace('i', ''))*1j
+
+
+def number_to_complex(s):
+    """
+    :param s: complex
+    :return: str
+    """
+    return f'{int(s.real)}+{int(s.imag)}i'
+
+
+print(complex_number_multiply('1+1i', '1+1i'))
+print(complex_number_multiply('1+-1i', '1+-1i'))
+# 0+2i
+# 0+-2i
