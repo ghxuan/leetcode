988653422e0a5c4374fdd72b08951502ac3f3e7a
ghxuan <ghxuan@outlook.com>
2018-12-03 10:44:49 +0800

    Excel表列序号

---
+++ py/title-to-number.py
@@ -0,0 +1,15 @@
+
+def titleToNumber(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    return sum([(ord(j)-64)*(26**i) for i, j in enumerate(s[::-1])])
+
+
+print(titleToNumber('A'))
+print(titleToNumber('AB'))
+print(titleToNumber('ZY'))
+# 1
+# 28
+# 701
