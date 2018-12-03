0e9d73d7590b531b6f8c1d6d8bdd0e2c52cffd9a
ghxuan <ghxuan@outlook.com>
2018-12-03 10:50:08 +0800

    有效数字

---
+++ py/is-number.py
@@ -0,0 +1,40 @@
+
+
+
+def isNumber(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    s = s.lower().strip(' ')
+    if ' ' in s:
+        return False
+    if 'e' in s:
+        strings = s.split('e')
+        try:
+            float(strings[0])
+            int(strings[1])
+        except Exception as e:
+            return False
+        else:
+            return True
+    try:
+        float(s)
+    except Exception as e:
+        return False
+    return True
+
+
+print(isNumber(' -0.5 '))
+print(isNumber(' 44.23 2 '))
+print(isNumber('abs'))
+print(isNumber(' -2.5E-10'))
+print(isNumber(' -+2.5E--10'))
+print(isNumber('1 a'))
+# True
+# False
+# False
+# True
+# False
+# False
+
