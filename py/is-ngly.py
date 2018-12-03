e50291d96287e6620fe8728b7a1859fcc5ef58b2
ghxuan <ghxuan@outlook.com>
2018-12-03 10:52:15 +0800

    丑数

---
+++ py/is-ngly.py
@@ -0,0 +1,34 @@
+
+def nthUgly(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    # if num == 1:
+    #     return True
+    res = num
+    while res % 2 == 0:
+        res /= 2
+    while res % 3 == 0:
+        res /= 3
+    while res % 5 == 0:
+        res /= 5
+    if res == 1:
+        return True
+    return False
+
+
+print(nthUgly(1))
+print(nthUgly(32))
+print(nthUgly(-2147483648))
+print(nthUgly(78732))
+print(nthUgly(600))
+print(nthUgly(2147483647))
+
+# True
+# True
+# False
+# True
+# True
+# False
+
