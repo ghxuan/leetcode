794b6393ea99e163c681fc80870cb3cfd7ae2ec2
ghxuan <ghxuan@outlook.com>
2018-12-03 10:40:56 +0800

    快乐数

---
+++ py/is-happy.py
@@ -0,0 +1,17 @@
+
+def isHappy(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    last = [n]
+    while n != 1:
+        n = sum([int(i)**2 for i in str(n)])
+        if n in last:
+            return False
+        last.append(n)
+    return True
+
+
+print(isHappy(19))
+# True
