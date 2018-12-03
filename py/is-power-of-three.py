6d10e9a2e14aef558194fe76f9e9d6f8990ab176
ghxuan <ghxuan@outlook.com>
2018-12-03 10:44:10 +0800

    3的幂

---
+++ py/is-power-of-three.py
@@ -0,0 +1,19 @@
+
+def isPowerOfThree(n):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    return n > 0 and not n ** 1/3 % 1
+
+
+print(isPowerOfThree(27))
+print(isPowerOfThree(9))
+print(isPowerOfThree(0))
+print(isPowerOfThree(45))
+print(isPowerOfThree(81))
+# True
+# True
+# False
+# True
+# True
