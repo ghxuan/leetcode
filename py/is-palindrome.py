ee2e45446ff08dc71ea0b9e605dabb94461a5350
ghxuan <ghxuan@outlook.com>
2018-12-03 10:42:16 +0800

    回文数

---
+++ py/is-palindrome.py
@@ -0,0 +1,23 @@
+
+def isPalindrome(x):
+    """
+    :type x: int
+    :rtype: bool
+    """
+    if x < 0 or not x % 10:
+        return False
+    res = []
+    while x:
+        x, i = divmod(x, 10)
+        res.append(i)
+    return not [i for i in range(len(res)//2+1) if res[i] != res[-1-i]]
+
+
+print(isPalindrome(121))
+print(isPalindrome(1221))
+print(isPalindrome(-121))
+print(isPalindrome(10))
+# True
+# True
+# False
+# False
