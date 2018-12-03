864ba3b2872131c9dcaa7371c6148111749735e9
ghxuan <ghxuan@outlook.com>
2018-12-03 10:44:30 +0800

    无重复字符的最长子串

---
+++ py/length-of-longest-substring.py
@@ -0,0 +1,24 @@
+
+
+def lengthOfLongestSubstring(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    longest_subs = longest_sub = ''
+    for i in s:
+        if i in longest_sub:
+            longest_sub = i
+        else:
+            longest_sub += i
+        if len(longest_sub) >= len(longest_subs):
+            longest_subs = longest_sub
+    print(longest_subs)
+    return len(longest_subs)
+
+
+print(lengthOfLongestSubstring('abcabcbb'))
+print(lengthOfLongestSubstring('bbbbb'))
+print(lengthOfLongestSubstring('pwwkew'))
+print(lengthOfLongestSubstring('pwwkeh'))
+
