966c801b19106b2f53a3f37e9ff93ae2ffddd6e4
ghxuan <ghxuan@outlook.com>
2018-12-03 10:42:52 +0800

    完美数

---
+++ py/check-perfect-number.py
@@ -0,0 +1,15 @@
+
+def checkPerfectNumber(num):
+    """
+    :type num: int
+    :rtype: bool
+    """
+    return not (sum(
+        [sum({i, divmod(num, i)[0]})
+         for i in range(2, int(num ** 0.5) + 1)
+         if not divmod(num, i)[1]]
+    ) + 1 - num if num != 1 else 1)
+
+
+print(checkPerfectNumber(28))
+# True
