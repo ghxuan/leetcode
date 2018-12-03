287b93de8df41e6039b876caaf7a4108d547493f
ghxuan <ghxuan@outlook.com>
2018-12-03 10:43:22 +0800

    范围求和 II

---
+++ py/max-count.py
@@ -0,0 +1,14 @@
+
+def maxCount(m, n, ops):
+    """
+    :type m: int
+    :type n: int
+    :type ops: List[List[int]]
+    :rtype: int
+    """
+    res = list(zip(*ops))
+    return min(res[0])*min(res[1])
+
+
+print(maxCount(3, 3, [[2, 2], [3, 3]]))
+# 4
