80c64af2c5701368d9a79ea0b9223bf56ac85ffc
ghxuan <ghxuan@outlook.com>
2018-12-03 10:43:13 +0800

    平方数之和

---
+++ py/judge-square-sum.py
@@ -0,0 +1,28 @@
+
+
+
+def judgeSquareSum(c):
+    """
+    :type c: int
+    :rtype: bool
+    """
+    i = 0
+    temp = int(c ** 0.5)
+    while i < temp:
+        num = i ** 2 + temp ** 2
+        if num > c:
+            temp -= 1
+        elif num < c:
+            i += 1
+        else:
+            print(i, temp)
+            return True
+    return False
+
+
+print(judgeSquareSum(5))
+print(judgeSquareSum(3))
+
+# True
+# False
+
