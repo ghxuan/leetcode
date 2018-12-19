344e80df0eb761cb3fbc8049ec847486eac8ce9f
ghxuan <ghxuan@outlook.com>
2018-12-19 23:34:09 +0800

    两数之和

--- py/two-sum.py
+++ py/two-sum.py
@@ -8,11 +8,19 @@ def two_sum(nums, target):
     #     # in 是 python 中比较方便的方法
     #     if target-i in nums:
     #         return [nums.index(i), nums.index(target-i)]
-    for i, j in enumerate(nums):
-        if nums[:i].count(target-j):
-            return [i, nums.index(target-j)]
+
+    # for i, j in enumerate(nums):
+    #     if nums[:i].count(target-j):
+    #         return [i, nums.index(target-j)]
+
+    res_hash = {}
+    for index, num in enumerate(nums):
+        if target - num in res_hash:
+            return [index, res_hash[target - num]]
+        res_hash[num] = index
 
 
 print(two_sum([2, 7, 11, 15], 9))
 # [0, 1]
+# [1, 0]
 # [1, 0]
