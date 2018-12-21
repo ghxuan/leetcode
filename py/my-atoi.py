0eeb0e7bc4d5a335fe7afb8f45d01fa703c59096
ghxuan <ghxuan@outlook.com>
2018-12-21 23:31:56 +0800

    字符串转换整数 (atoi)

--- py/my-atoi.py
+++ py/my-atoi.py
@@ -1,37 +1,50 @@
-def myAtoi(str):
+import re
+
+
+def my_atoi(string):
     """
-    :type str: str
+    :type string: str
     :rtype: int
     """
-    num = str.strip(' ')
-    if len(num) == 0:
+    # num = string.strip(' ')
+    # temp, n = '+-0123456789', len(num)
+    # if n > 0 and num[0] in temp:
+    #     temp = temp[2:]
+    #     for i in range(1, n):
+    #         if num[i] not in temp:
+    #             num = num[:i]
+    #             break
+    #     num = 0 if len(num) == 1 and num[0] in '-+' or ('+' in num and '-' in num) else int(num.replace('+', ''))
+    #     if num < -2147483648:
+    #         return -2147483648
+    #     elif num > 2147483647:
+    #         return 2147483647
+    #     return num
+    # return 0
+
+    string = string.strip()
+    try:
+        res = int(re.search('^[+-]?\d+', string).group())
+        res = res if res <= 2147483647 else 2147483647
+        res = res if res >= -2147483648 else -2147483648
+    except:
         return 0
-    temp = '-+123456789'
-    if (num[0] in temp[:2] and num[1] != '0') or num[0] in temp[2:]:
-        temp = temp[2:] + '0'
-        for i in range(1, len(num)):
-            if num[i] not in temp:
-                num = num[:i]
-                break
-        num = int(num)
-        if num < -2147483648:
-            return -2147483648
-        elif num > 2147483647:
-            return 2147483647
-        return num
-    return 0
+    return res
 
 
-print(myAtoi('42'))
-print(myAtoi('   -042'))
-print(myAtoi('4193 with words'))
-print(myAtoi('words and 987'))
-print(myAtoi('-91283472332'))
-print(myAtoi(''))
-
+print(my_atoi('42'))
+print(my_atoi('   -042'))
+print(my_atoi('4193 with words'))
+print(my_atoi('words and 987'))
+print(my_atoi('-91283472332'))
+print(my_atoi(''))
+print(my_atoi("  0000000000012345678"))
+print(my_atoi('-'))
 # 42
-# 0
+# -42
 # 4193
 # 0
 # -2147483648
+# 0
+# 12345678
 # 0
