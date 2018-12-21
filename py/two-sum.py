dc92cd88c7a3ece910ae1477aeb47051572395e0
ghxuan <ghxuan@outlook.com>
2018-12-21 20:12:15 +0800

    两数之和

--- py/two-sum.py
+++ py/two-sum.py
@@ -4,20 +4,13 @@ def two_sum(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    # for i in nums:
-    #     # in 是 python 中比较方便的方法
-    #     if target-i in nums:
-    #         return [nums.index(i), nums.index(target-i)]
-
-    # for i, j in enumerate(nums):
-    #     if nums[:i].count(target-j):
-    #         return [i, nums.index(target-j)]
-
     res_hash = {}
     for index, num in enumerate(nums):
-        if target - num in res_hash:
-            return [index, res_hash[target - num]]
-        res_hash[num] = index
+        if num not in res_hash:
+            res_hash[target-num] = index
+        else:
+            return [index, res_hash[num]]
+
 
 
 print(two_sum([2, 7, 11, 15], 9))
