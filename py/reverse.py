e45f3afca18c63c8faed13dd3de8ef94f9c3aec9
ghxuan <ghxuan@outlook.com>
2018-12-22 11:19:04 +0800

    整数反转

--- py/reverse.py
+++ py/reverse.py
@@ -3,12 +3,11 @@ def reverse(x):
     :type x: int
     :rtype: int
     """
-    x = str(x).strip('0')
-    if '-' in x:
-        x = -int(x[:0:-1])
+    if x < 0:
+        x = -int(str(x)[-1:0:-1])
     else:
-        x = int(x[::-1])
-    if -2**31 <= x <= 2**31-1:
+        x = int(str(x)[::-1])
+    if -2147483648 <= x <= 2147483647:
         return x
     return 0
 
@@ -16,7 +15,7 @@ def reverse(x):
 print(reverse(123))
 print(reverse(-123))
 print(reverse(-120))
-print(reverse(2**31))
+print(reverse(2 ** 31))
 # 321
 # -321
 # -21
