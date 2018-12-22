690bf5322c28c8aa2cb233a834d995a3b7fa4713
ghxuan <ghxuan@outlook.com>
2018-12-22 11:21:23 +0800

    正则表达式匹配

--- py/is-match.py
+++ py/is-match.py
@@ -1,46 +1,14 @@
+import re
+
+
 def is_match(s, p):
     """
     :type s: str
     :type p: str
     :rtype: bool
     """
-    left, right = count(s), count(p)
-    # print(left, right)
-    index = 0
-    p, q = 0, 0
-    for i in left:
-        m, n = i.popitem()
-        if not q.imag:
-            p, q = right[index].popitem()
-        # print(m, n, p, q)
-        if m == p or p == '.':
-            if q.imag:
-                if q.real - 1 > n:
-                    return False
-            elif q.real < n:
-                return False
-            else:
-                index += 1
-        elif q.imag:
-            q = 0
-            index += 1
-        else:
-            return False
-    return True
-
-
-def count(s):
-    next_ = ''
-    list_ = []
-    for i in s:
-        if i == next_:
-            list_[len(list_) - 1][i] += 1
-        elif i == '*':
-            list_[len(list_) - 1][next_] += 1j
-        else:
-            list_.append({i: 1})
-            next_ = i
-    return list_
+    value = re.match(p, s)
+    return False if value is None or value.group(0) != s else True
 
 
 print(is_match('aa', 'a'))
@@ -49,9 +17,11 @@ print(is_match('ab', '.*'))
 print(is_match('aab', 'c*a*b'))
 print(is_match('mississippi', 'miss*is*p*.'))
 print(is_match('', ''))
+print(is_match("abcd", "d*"))
 # False
 # True
 # True
 # True
 # False
 # True
+# False
