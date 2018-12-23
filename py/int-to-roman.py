1bc80fc917d23f981a8d5f9752aa871163b6e178
ghxuan <ghxuan@outlook.com>
2018-12-23 23:41:32 +0800

    整数转罗马数字

--- py/int-to-roman.py
+++ py/int-to-roman.py
@@ -1,29 +1,38 @@
-def intToRoman(num):
+def int_to_roman(num):
     """
     :type num: int
     :rtype: str
     """
-    int_dict = {
-        1: 'I', 4: 'IV', 5: 'V', 9: 'IX',
-        10: 'X', 40: 'XL', 50: 'L', 90: 'XC',
-        100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
-    }
+    # dict_ = {
+    #     1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C',
+    #     90: 'XC', 50: 'L', 40: 'XL', 10: 'X',
+    #     9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
+    # }
+    # res = ''
+    # for key, value in dict_.items():
+    #     if num >= key:
+    #         temp, num = divmod(num, key)
+    #         res += value * temp
+    # return res
+
+    list_ = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'),
+        (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+        (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
     res = ''
-    keys = list(int_dict.keys())[::-1]
-    values = list(int_dict.values())[::-1]
-    for i, j in enumerate(keys):
-        while num >= j:
-            temp = num // j
-            num -= j * temp
-            res += values[i] * temp
+    for key, value in list_:
+        while num >= key:
+            num -= key
+            res += value
     return res
 
 
-print(intToRoman(3))
-print(intToRoman(4))
-print(intToRoman(9))
-print(intToRoman(58))
-print(intToRoman(1994))
+print(int_to_roman(3))
+print(int_to_roman(4))
+print(int_to_roman(9))
+print(int_to_roman(58))
+print(int_to_roman(1994))
 # III
 # IV
 # IX
