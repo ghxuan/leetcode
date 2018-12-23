a17ba0843b819a250935d953a30e362319712088
ghxuan <ghxuan@outlook.com>
2018-12-23 23:46:04 +0800

    正则表达式匹配

--- py/is-match.py
+++ py/is-match.py
@@ -7,8 +7,27 @@ def is_match(s, p):
     :type p: str
     :rtype: bool
     """
-    value = re.match(p, s)
-    return False if value is None or value.group(0) != s else True
+    # value = re.match(p, s)
+    # return False if value is None or value.group(0) != s else True
+
+    def dfs(string, pattern, memo):
+        if not pattern and string:
+            return False
+        if not string and pattern:
+            return set(pattern[1::2]) == {"*"} and not (len(pattern) % 2)
+        if (string, pattern) in memo:
+            return memo[string, pattern]
+        char, exp, prev = string[-1], pattern[-1], 0 if len(pattern) < 2 else pattern[-2]
+        memo[string, pattern] = (
+                         exp == '*' and
+                         (
+                             (prev in {char, '.'} and dfs(string[:-1], pattern, memo)) or
+                             dfs(string, pattern[:-2], memo)
+                         )
+                      ) or (exp in {char, '.'} and dfs(string[:-1], pattern[:-1], memo))
+        return memo[string, pattern]
+
+    return dfs(s, p, memo={("", ""): True})
 
 
 print(is_match('aa', 'a'))
