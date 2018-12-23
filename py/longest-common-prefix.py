3bda20f54342643fb481e51f04c0ea40c3f7e0a2
ghxuan <ghxuan@outlook.com>
2018-12-23 23:33:00 +0800

    最长公共前缀

--- py/longest-common-prefix.py
+++ py/longest-common-prefix.py
@@ -3,23 +3,29 @@ def longest_common_prefix(strings):
     :type strings: List[str]
     :rtype: str
     """
-    strings = sorted(strings)
-    # print(strings)
-    # python 自带的 sorted 方法
-    # 是以 ASCII 排序的，
-    # 那么，如果最后一个和第一个相等，
-    # 则说明 所有内容相等
-    start = strings[0]
-    end = strings[1]
-    for i, _ in enumerate(start):
-        if not end.startswith(start[:i]):
-            return f'"{end[:i-1]}"'
-    return f'"{start}"'
+    # if len(strings) == 0:
+    #     return ""
+    # res = strings[0]
+    # for string in strings[1:]:
+    #     while not string.startswith(res):
+    #         res = res[:-1]
+    #         if res == "":
+    #             return res
+    # return res
+
+    strings = list(zip(*strings))
+    res = ''
+    for string in strings:
+        if len(set(string)) == 1:
+            res += string[0]
+        else:
+            break
+    return res
 
 
 print(longest_common_prefix(["flower", "flow", "flight"]))
 print(longest_common_prefix(["dog", "racecar", "car"]))
 print(longest_common_prefix(["dog", "dogs", "dog_pp"]))
-# "fl"
-# ""
-# "dog"
+# fl
+#
+# dog
