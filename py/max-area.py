0b4a6c61d2bbdcddab5a2b28704c80c425524880
ghxuan <ghxuan@outlook.com>
2018-12-23 23:39:09 +0800

    盛最多水的容器

--- py/max-area.py
+++ py/max-area.py
@@ -5,14 +5,27 @@ def max_area(height):
     """
     res, i, j = 0, 0, len(height) - 1
     while i < j:
-        h = min(height[i], height[j])
-        res = max(res, h * (j - i))
-        while h == height[i]:
+        # h = min(height[i], height[j])
+        # area = h * (j - i)
+        # if area > res:
+        #     res = area
+        # while h >= height[i] and i < j:
+        #     i += 1
+        # while h >= height[j] and i < j:
+        #     j -= 1
+
+        if height[i] < height[j]:
+            area = height[i] * (j - i)
             i += 1
-        while h == height[j]:
+        else:
+            area = height[j] * (j - i)
             j -= 1
+        if area > res: 
+            res = area
     return res
 
 
 print(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(max_area([1, 1]))
 # 49
+# 1
