05e184b19d02fd078b37834513d6c93a4c70eb5e
ghxuan <ghxuan@outlook.com>
2018-12-23 23:38:36 +0800

    罗马数字转整数

--- py/roman-to-int.py
+++ py/roman-to-int.py
@@ -1,26 +1,25 @@
-def romanToInt(s):
+def roman_to_int(s):
     """
     :type s: str
     :rtype: int
     """
-    int_dict = {
+    dict_ = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
-    sum_s = 0
-    for i in range(len(s)):
-        sum_s += int_dict[s[i]]
-        if int_dict[s[i]] > int_dict[s[i-1]]:
-            sum_s -= 2*int_dict[s[i-1]]
-    return sum_s
+    res, temp = 0, 0
+    for string in s[::-1]:
+        cur = dict_[string]
+        res, temp = res - cur if cur < temp else res + cur, cur
+    return res
 
 
-print(romanToInt('III'))
-print(romanToInt('IV'))
-print(romanToInt('IX'))
-print(romanToInt('LVIII'))
-print(romanToInt('MCMXCIV'))
+print(roman_to_int('III'))
+print(roman_to_int('IV'))
+print(roman_to_int('IX'))
+print(roman_to_int('LVIII'))
+print(roman_to_int('MCMXCIV'))
 # 3
 # 4
 # 9
-# 56
-# 1984
+# 58
+# 1994
