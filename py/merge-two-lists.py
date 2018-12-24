4c65a39f40c109aca6fda971487049a81d227143
ghxuan <ghxuan@outlook.com>
2018-12-24 13:40:20 +0800

    合并两个有序链表

--- py/merge-two-lists.py
+++ py/merge-two-lists.py
@@ -46,6 +46,19 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+
+        # head = temp = ListNode(0)
+        # while l1 and l2:
+        #     if l1.val <= l2.val:
+        #         temp.next = l1
+        #         l1 = l1.next
+        #     else:
+        #         temp.next = l2
+        #         l2 = l2.next
+        #     temp = temp.next
+        # temp.next = l1 or l2
+        # return head.next
+
         if not l1:
             return l2
         if not l2:
