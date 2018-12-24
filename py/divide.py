8f1bcf41e8d4675c86e8c9a744b29f9a5aa48ce6
ghxuan <ghxuan@outlook.com>
2018-12-24 13:39:26 +0800

    两数相除

--- py/divide.py
+++ py/divide.py
@@ -4,20 +4,27 @@ def divide(dividend, divisor):
     :type divisor: int
     :rtype: int
     """
-    if not divisor or (divisor == 1 and dividend == -2147483648):
-        return 2147483647
-    end, sor = abs(dividend), abs(divisor)
+    int_max, int_min = 2147483647, -2147483648
+    sign = 1
+    if 0 in [dividend, divisor]:
+        return 0
+    elif dividend < 0 < divisor or divisor < 0 < dividend:
+        sign = -1
+        dividend, divisor = abs(dividend), abs(divisor)
+    else:
+        dividend, divisor = abs(dividend), abs(divisor)
     res = 0
-    while end > sor:
-        end -= sor
-        res += 1
-    return res if plus_or_minus(dividend) == plus_or_minus(divisor) else -res
-
-
-def plus_or_minus(x):
-    if abs(x) == x:
-        return True
-    return False
+    while dividend >= divisor:
+        tmp, val = divisor, 1
+        while dividend >= tmp:
+            res += val
+            dividend -= tmp
+            tmp += tmp
+            val += val
+    if sign == 1:
+        return min(int_max, res)
+    else:
+        return max(int_min, 0 - res)
 
 
 print(divide(10, 3))
