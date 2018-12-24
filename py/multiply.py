ad12d964d26825dba6424a439f719d26cc7a42c4
ghxuan <ghxuan@outlook.com>
2018-12-24 23:45:36 +0800

    字符串相乘

--- py/multiply.py
+++ py/multiply.py
@@ -4,16 +4,27 @@ def multiply(num1, num2):
     :type num2: str
     :rtype: str
     """
-    res = 0
-    for n, i in enumerate(num1[::-1]):
-        for m, j in enumerate(num2[::-1]):
-            res += int(i)*int(j)*10**(m+n)
-    return str(res)
+    dict_ = {
+        '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+    }
+    res, n1, n2 = 0, 0, 0
+    # for n, i in enumerate(num1[::-1]):
+    #     for m, j in enumerate(num2[::-1]):
+    #         res += dict_[i] * dict_[j] * 10 ** (m + n)
+    # return str(res)
+    for i in num1:
+        n1 = dict_[i] + n1 * 10
+    for j in num2:
+        n2 = dict_[j] + n2 * 10
+    return str(n1 * n2)
 
 
 print(multiply('1', '1'), 1)
-print(multiply('12', '12'), 12*12)
-print(multiply('156', '156'), 156*156)
+print(multiply('12', '12'), 12 * 12)
+print(multiply('156', '156'), 156 * 156)
+print(multiply("123", "456"), 123 * 456)
 # 1 1
 # 144 144
 # 24336 24336
+# 56088 56088
