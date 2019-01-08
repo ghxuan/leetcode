d5ef573650ddf47c35aa99c37a8c7eb2aeecd061
ghxuan <ghxuan@outlook.com>
2019-01-08 23:33:22 +0800

    与所有单词相关联的字串

--- py/find-substring.py
+++ py/find-substring.py
@@ -1,27 +1,48 @@
-from itertools import permutations
-from functools import reduce
-
-
 def find_substring(s, words):
     """
     :type s: str
     :type words: List[str]
     :rtype: List[int]
     """
-    res = []
-    for i in permutations(words):
-        res += index(s, reduce(lambda x, y: x + y, i))
+    length = dict((word, len(word)) for word in words)
+    lens, lenw, lenws = len(s), len(words[0]), len(words)
+    longest, temp, left, cur, res = lens - lenw + 1, [False] * lenws, 0, 0, []
+    while cur < longest:
+        string = s[cur:cur + lenw]
+        if string in length:
+            temp[left] = [cur]
+            left += 1
+            if all(temp):
+                res.append(temp[0][0])
+            cur += lenw
+        else:
+            left, temp = 0, [False] * lenws
+            cur += 1
     return res
 
+    
+# from itertools import permutations
+# from functools import reduce
+# def find_substring(s, words):
+#     """
+#     :type s: str
+#     :type words: List[str]
+#     :rtype: List[int]
+#     """
+#     res = []
+#     for i in permutations(words):
+#         res += index(s, reduce(lambda x, y: x + y, i))
+#     return res
 
-def index(s, p):
-    count = 0
-    res = []
-    while p in s[count:]:
-        index_ = count+s[count:].index(p)
-        res.append(index_)
-        count = index_+len(p)
-    return res
+
+# def index(s, p):
+#     count = 0
+#     res = []
+#     while p in s[count:]:
+#         index_ = count+s[count:].index(p)
+#         res.append(index_)
+#         count = index_+len(p)
+#     return res
 
 
 print(find_substring('barfoothefoobarman', ["foo", "bar"]))
