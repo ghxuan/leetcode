ebdc5a057a5cb4056dc74831c8b3ddedee6e1462
ghxuan <ghxuan@outlook.com>
2019-02-28 23:32:55 +0800

    下一个排列

--- py/next-permutation.py
+++ py/next-permutation.py
@@ -1,36 +1,49 @@
 def next_permutation(nums):
     """
     :type nums: List[int]
-    :rtype: void Do not return anything, modify nums in-place instead.
+    :rtype: None Do not return anything, modify nums in-place instead.
     """
-    n = len(nums)
-    i = n - 1
-    j = n - 1
-    while i:
-        # 找到出现转折的地方
-        if nums[i] > nums[i - 1]:
-            cur = i - 1
-            temp = nums[cur]
-            nums[cur:] = sorted(nums[cur:])
-            while j:
-                # 从后向前 从已排序中找到比 temp 大一点的数
-                if temp == nums[j]:
-                    nums.insert(cur, nums.pop(j+1))
-                    print(nums)
-                    return
-                j -= 1
+    j = len(nums) - 1
+    i = j - 1
+    last, n_last = nums[j], nums[i]
+    while i != -1 and n_last >= last:
         i -= 1
-    nums.sort()
+        last, n_last = n_last, nums[i]
+    if i != -1:
+        last = nums[j]
+        while last <= n_last:
+            j -= 1
+            last = nums[j]
+        nums[i], nums[j] = last, n_last
+    nums[i + 1:] = sorted(nums[i + 1:])
+    # n = len(nums)
+    # j = n - 1
+    # i = j - 1
+    # nums_s = dict(zip(range(n), nums))
+    # last, n_last = nums_s.get(j), nums_s.get(i)
+    # while i != -1 and n_last >= last:
+    #     i -= 1
+    #     last, n_last = n_last, nums_s.get(i, 0)
+    # if i != -1:
+    #     last = nums_s.get(j)
+    #     while last <= n_last:
+    #         j -= 1
+    #         last = nums_s.get(j)
+    #     nums[i], nums[j] = last, n_last
+    # nums[i + 1:] = sorted(nums[i + 1:])
     print(nums)
 
 
 next_permutation([1, 2, 3])
 next_permutation([3, 2, 1])
 next_permutation([1, 1, 5])
-next_permutation([1, 2, 7, 4, 3, 1])
-next_permutation([1, 2, 7, 4, 1, 3])
+next_permutation([1, 3, 2])
+next_permutation([2, 3, 1])
+next_permutation([1, 2])
+
 # [1, 3, 2]
 # [1, 2, 3]
 # [1, 5, 1]
-# [1, 3, 1, 2, 4, 7]
-# [1, 2, 7, 4, 3, 1]
+# [2, 1, 3]
+# [3, 1, 2]
+# [2, 1]
