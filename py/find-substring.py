1b2581e642470689034b608e743b973f18b5bdee
ghxuan <ghxuan@outlook.com>
2019-02-28 23:33:54 +0800

    与所有单词相关联的字串

--- py/find-substring.py
+++ py/find-substring.py
@@ -4,48 +4,36 @@ def find_substring(s, words):
     :type words: List[str]
     :rtype: List[int]
     """
-    length = dict((word, len(word)) for word in words)
-    lens, lenw, lenws = len(s), len(words[0]), len(words)
-    longest, temp, left, cur, res = lens - lenw + 1, [False] * lenws, 0, 0, []
-    while cur < longest:
-        string = s[cur:cur + lenw]
-        if string in length:
-            temp[left] = [cur]
-            left += 1
-            if all(temp):
-                res.append(temp[0][0])
-            cur += lenw
-        else:
-            left, temp = 0, [False] * lenws
-            cur += 1
+    if not s or not words:
+        return []
+    lens, len_word, len_subs, times = len(s), len(words[0]), 0, {}
+    times = {}
+    for word in words:
+        len_subs += len_word
+        times[word] = times.get(word) + 1 if times.get(word) else 1
+    res = []
+    for i in range(min(len_word, lens - len_subs + 1)):
+        start = i
+        cur = {}
+        while i + len_subs <= lens:
+            temp = start
+            start += len_word
+            word = s[temp:start]
+            if word not in times:
+                i = start
+                cur.clear()
+            else:
+                cur[word] = cur.get(word) + 1 if cur.get(word) else 1
+                while cur[word] > times[word]:
+                    temp = i
+                    i += len_word
+                    cur[s[temp:i]] -= 1
+                if start - i == len_subs:
+                    res.append(i)
     return res
-
-    
-# from itertools import permutations
-# from functools import reduce
-# def find_substring(s, words):
-#     """
-#     :type s: str
-#     :type words: List[str]
-#     :rtype: List[int]
-#     """
-#     res = []
-#     for i in permutations(words):
-#         res += index(s, reduce(lambda x, y: x + y, i))
-#     return res
-
-
-# def index(s, p):
-#     count = 0
-#     res = []
-#     while p in s[count:]:
-#         index_ = count+s[count:].index(p)
-#         res.append(index_)
-#         count = index_+len(p)
-#     return res
 
 
 print(find_substring('barfoothefoobarman', ["foo", "bar"]))
 print(find_substring('wordgoodstudentgoodword', ["word", "student"]))
-# [9, 0]
+# [0, 9]
 # []
