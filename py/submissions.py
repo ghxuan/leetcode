9f2e722cbfff30674c73be5c573257d4c60e5249
ghxuan <ghxuan@outlook.com>
2019-10-28 09:53:26 +0800

    Update submissions.py

--- py/submissions.py
+++ py/submissions.py
@@ -1,9 +1,15 @@
-from typing import List, Set
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: [int]) -> [[int]]:
         if not len(nums): return [[]]
         res = []
         for sub in self.subsets(nums[1:]):
             res.append(sub)
             res.append([nums[0]]+ sub)
+        return res
+
+class Solution:
+    def subsets(self, nums: [int]) -> [[int]]:
+        res = []
+        for i in range(2 ** len(nums)):
+            res.append([nums[x] for x, y in enumerate(bin(i)[:1:-1]) if y == '1'])
         return res
